3afaac8d99e06a0a675afc2d7789130f3dcc3e66
Austin Hurst <austinhurst@msn.com>
2018-03-20 10:32:48 -0600

    3D plotter works

--- src/static_plotter.py
+++ src/static_plotter.py
@@ -8,7 +8,10 @@ from theseus.msg import AuvsiBoundaries
 from rosplane_msgs.msg import Waypoint
 from rosplane_msgs.msg import State
 import matplotlib.pyplot as plt
+from matplotlib.collections import PolyCollection
+import mpl_toolkits.mplot3d.art3d as art3d
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.patches import Circle
 import matplotlib.animation as animation
 import numpy as np
 import time
@@ -18,8 +21,8 @@ class path_plotter:
 
         # PLOTTING VARIABLES
         self.plot_update_interval = 10
-        fig, ax = plt.subplots()
-        self.fig = fig
+        self.fig = plt.figure()
+        self.ax = self.fig.gca(projection='3d')
         self.ani = animation.FuncAnimation(self.fig, self.plot_data, interval=self.plot_update_interval)
 
         # ROS SUBSCRIPTIONS
@@ -47,14 +50,9 @@ class path_plotter:
     def plot_data(self, unkown_variable):
         # plt.axis('equal')
         if self.update_map:
-            # Plot Obstacles
-            numObs = len(self.map_msg.obs)
-            for x in self.map_msg.obs:
-                CE = x.east
-                CN = x.north
-                R  = x.radius
-                H  = x.height
-                plt.fill(CE + R*np.cos(self.theta), CN + R*np.sin(self.theta), "r")
+            plt.ylim(-400,1200)
+            plt.xlim(-600,1000)
+            # plt.fill([-600, -600, 1000, 1000], [-400, 1200, 1200, -400], "g")
             # Plot boundaries
             xb = []
             yb = []
@@ -63,19 +61,51 @@ class path_plotter:
                 yb.append(x.north)
             xb.append(xb[0])
             yb.append(yb[0])
-            plt.plot(xb,yb)
+            # plt.fill(xb, yb, "w")
+            plt.plot(xb,yb,zs=0)
+            # Plot Obstacles
+            verts = []
+            walls = []
+            wallzs = []
+            zs = []
+            numObs = len(self.map_msg.obs)
+            for x in self.map_msg.obs:
+                CE = x.east
+                CN = x.north
+                R  = x.radius
+                H  = x.height
+                resolution = 100
+                x = np.linspace(CE-R, CE+R, resolution)
+                z = np.linspace(0, H, resolution)
+                X, Z = np.meshgrid(x, z)
+
+                Y = np.sqrt(R**2 - (X - CE)**2) + CN # Pythagorean theorem
+
+                self.ax.plot_surface(X, Y, Z, linewidth=0, color='r')
+                self.ax.plot_surface(X, (2*CN-Y), Z, linewidth=0, color='r')
+
+                floor = Circle((CE, CN), R, color='r')
+                self.ax.add_patch(floor)
+                art3d.pathpatch_2d_to_3d(floor, z=0, zdir="z")
+
+                ceiling = Circle((CE, CN), R, color='r')
+                self.ax.add_patch(ceiling)
+                art3d.pathpatch_2d_to_3d(ceiling, z=H, zdir="z")
+
             # Plot Major Waypoints
             xwps = []
             ywps = []
             dwps = []
+            hwps = []
             numwp = 1
             for x in self.map_msg.primary_wps:
                 xwps.append(x.w[1])
                 ywps.append(x.w[0])
                 dwps.append(x.w[2])
-                plt.annotate(str(numwp),[x.w[1] + 15 , x.w[0] + 15]);
+                hwps.append(-1.0*x.w[2])
+                self.ax.text(x.w[1] + 15 , x.w[0] + 15, -1.0*x.w[2] + 15, str(numwp));
                 numwp = numwp + 1
-            plt.scatter(xwps,ywps,marker="x",s=200, linewidths=4)
+            self.ax.scatter(xwps,ywps,hwps, marker="x",s=200, depthshade=False, linewidths=4)
             # Plot all Waypoints
             xwpsAll = []
             ywpsAll = []
@@ -84,13 +114,14 @@ class path_plotter:
                 xwpsAll.append(x.w[1])
                 ywpsAll.append(x.w[0])
                 dwpsAll.append(x.w[2])
-
             mav_path = self.plot_mav_path(xwps, ywps, dwps, xwpsAll, ywpsAll, dwpsAll)
-            plt.plot(mav_path[:len(mav_path)/2][0], mav_path[len(mav_path)/2:][0], linewidth=4)
-
+            xallwps = [i * 1.0 for i in mav_path[0][0]]
+            yallwps = [i * 1.0 for i in mav_path[1][0]]
+            hallwps = [i * -1.0 for i in mav_path[2][0]]
+            self.ax.plot(xallwps, yallwps, hallwps,'k', linewidth=4)
             self.update_map = False
         if self.update_plane_position:
-            plt.plot(self.Es,self.Ns,'k',linewidth=2)
+            plt.plot(self.Es,self.Ns,self.Ds,'g',linewidth=2)
             self.Es = []
             self.Ns = []
             self.Ds = []
@@ -116,7 +147,7 @@ class path_plotter:
             allWPS_plus_arc[1] += path_data[1]
             allWPS_plus_arc[2] += path_data[2]
             wp_index = j + 1;
-        return [allWPS_plus_arc[0], allWPS_plus_arc[1]]
+        return [[allWPS_plus_arc[0]], [allWPS_plus_arc[1]], [allWPS_plus_arc[2]]]
 
     def fillet_path(self, x_path_data, y_path_data, d_path_data):
         path_data_new = [[x_path_data[0]], [y_path_data[0]], [d_path_data[0]]];
